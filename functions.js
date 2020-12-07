a7b82fbc7c208d36adca8dc2c547986f54b794e6
Cozma Cristian <cozmacristian@HUAWEI_P9_lite_mini.localdomain>
2020-12-07 21:19:43 +0200

    arrow dn wirhour parenthesis

--- functions.js
+++ functions.js
@@ -35,7 +35,7 @@ function showPage(pageId) {
 }
 
 function initMenu() {
-    document.addEventListener("click", (e) => {
+    document.addEventListener("click", e => {
         const link = e.target;
         if (link.matches("#top-menu-bar a")) {
             const id = link.getAttribute("data-page");
@@ -49,7 +49,7 @@ initMenu();
 showPage(activePage);
 
 function getHTMLSkills(skills) {
-    return skills.map((skill) => {
+    return skills.map(skill => {
         return `<li class="${skill.endorsements > 9 ? "favorite" : ""}">
          ${skill.name}  <span>&middot; ${skill.endorsements}</span>
          </li>`;
@@ -61,7 +61,7 @@ function showSkills(skills) {
     ul.innerHTML = getHTMLSkills(skills);;
 }
 
-fetch("data/skills.json").then((r) => {
+fetch("data/skills.json").then(r => {
     return r.json();
 }).then((allSkills) => {
     allSkills.sort((s1, s2) => {
